bf060588a0ba7c4939ffe250af114632e010f832
Wittawat Fuangfu <wittawat335@gmail.com>
2024-01-21 13:56:12 +0700

    npm i zod

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,8 +1,16 @@
 import { useLogin } from "@/services/mutations/authMutation";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function SignIn() {
   const { mutateAsync: login, isPending } = useLogin();
+  const form = useForm<z.infer<typeof SigninValidation>>({
+    resolver: zodResolver(SigninValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <div>
       {" "}
