3e7b7c12d7313b2248f022628e478fbe792db80d
Wittawat Fuangfu <wittawat335@gmail.com>
2024-01-21 13:43:43 +0700

    install tailwind

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -2,6 +2,11 @@ import { useLogin } from "@/services/mutations/authMutation";
 import React from "react";
 
 export default function SignIn() {
-  const {mutateAsync:login, isPending} = useLogin();
-  return <div>SignIn</div>;
+  const { mutateAsync: login, isPending } = useLogin();
+  return (
+    <div>
+      {" "}
+      <h1 className="">Hello world!</h1>
+    </div>
+  );
 }
