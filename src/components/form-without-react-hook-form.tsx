2c26a5cac01d819435277b88426f6fc407e41c95
Wittawat Fuangfu <wittawat335@gmail.com>
2024-01-28 17:04:16 +0700

    #

--- src/components/form-without-react-hook-form.tsx
+++ src/components/form-without-react-hook-form.tsx
@@ -1,3 +1,4 @@
+import { Stack, Typography } from "@mui/material";
 import React, { useState } from "react";
 
 export default function FormWithoutReactHookForm() {
@@ -29,50 +30,55 @@ export default function FormWithoutReactHookForm() {
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-y-2">
-      {errors.length > 0 && (
-        <ul>
-          {errors.map((error) => (
-            <li
-              key={error}
-              className="bg-red-100 text-red-500 px-4 py-2 rounded"
-            >
-              {error}
-            </li>
-          ))}
-        </ul>
-      )}
-      <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        type="email"
-        required
-        placeholder="Email"
-        className="px-4 py-2 rounded"
-      />
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-        required
-        placeholder="Password"
-        className="px-4 py-2 rounded"
-      />
-      <input
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-        type="password"
-        required
-        placeholder="Confirm password"
-        className="px-4 py-2 rounded"
-      />
+      <Typography variant="h6" color={"blue"}>
+        FormWithoutReactHookForm
+      </Typography>
+      <Stack spacing={2} margin={2} width={400}>
+        {errors.length > 0 && (
+          <ul>
+            {errors.map((error) => (
+              <li
+                key={error}
+                className="bg-red-100 text-red-500 px-4 py-2 rounded"
+              >
+                {error}
+              </li>
+            ))}
+          </ul>
+        )}
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          required
+          placeholder="Email"
+          className="px-4 py-2 rounded"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          required
+          placeholder="Password"
+          className="px-4 py-2 rounded"
+        />
+        <input
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          type="password"
+          required
+          placeholder="Confirm password"
+          className="px-4 py-2 rounded"
+        />
 
-      <button
-        type="submit"
-        disabled={isSubmitting}
-        className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
-      >
-        Submit
-      </button>
+        <button
+          type="submit"
+          disabled={isSubmitting}
+          className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
+        >
+          Submit
+        </button>
+      </Stack>
     </form>
   );
 }
